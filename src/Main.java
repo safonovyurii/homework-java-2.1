aa274ed0c3562ee0c57b89ead95e7b467620022f
safonov yurii <safonov-yurii89@mail.ru>
2023-03-21 19:05:32 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int cost = 20000;
+        boolean data = true;
+
+        int mile = 20;
+
+        int bonus = cost / mile;
+
+        System.out.println("Бонусные милли:" + bonus);
+
+
+    }
+}
